8c1aded6dde5d48edd318636c3aa2e9b017f35ae
AlijonovAzamjon2303 <112892881+AlijonovAzamjon2303@users.noreply.github.com>
2024-11-09 14:06:12 +0500

    Ism familiya formasi qo'yildi va u faylga saqlash funksiyasi qo'shildi

--- pagesapp/urls.py
+++ pagesapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, name_view
+from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('save-name/', views.name_view, name='save_name'),
 ]
--- pagesapp/views.py
+++ pagesapp/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponse
+from .forms import NameForm
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+def save_name(first_name, last_name):
+    with open('names.txt', 'a') as file:
+        file.write(f"{first_name} {last_name}")
+
+
+def name_view(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+
+            # Ma'lumotni faylga yozamiz
+            save_name(first_name, last_name)
+
+            # Yuborilgan ma'lumotni ko'rsatish
+            return HttpResponse(f"Data saved: {first_name} {last_name}")
+    else:
+        form = NameForm()
+
+    return render(request, 'home.html', {'form': form})
